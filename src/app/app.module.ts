800c195fe2b29f3832a83ca8ea53f7be1371f39d
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-11 15:36:18 +0300

    create app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,30 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { QuotesGenComponent } from "./quotes-gen/quotes-gen.component";
-import { FormComponent } from "./form/form.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatInputModule } from "@angular/material/input";
-import { FormsModule } from "@angular/forms";
-import { VoteComponent } from "./vote/vote.component";
-import { QuoteDetailsComponent } from "./quote-details/quote-details.component";
-import { TimerCounterPipe } from "./timer-counter.pipe";
-import { HighlightDirective } from "./highlight.directive";
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { HeroComponent } from './hero/hero.component';
+import { FormComponent } from './form/form.component';
+import { ItemComponent } from './item/item.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { FormsModule } from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesGenComponent,
+    LogoComponent,
+    HeroComponent,
     FormComponent,
-    VoteComponent,
-    QuoteDetailsComponent,
-    TimerCounterPipe,
-    HighlightDirective,
+    ItemComponent,
+    QuoteDetailComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
